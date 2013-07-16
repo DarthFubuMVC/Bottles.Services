c5277637427f710cbf59fe1b3ce5a002f8852982
jeremydmiller <jeremydmiller@yahoo.com>
2013-07-16 15:20:24 -0500

    little bit of tracking code for messages in and out

--- src/Bottles.Services/Messaging/Tracking/TrackingClasses.cs
+++ src/Bottles.Services/Messaging/Tracking/TrackingClasses.cs
@@ -1,22 +1,77 @@
 ﻿using System;
+using System.Linq;
+using FubuCore;
 
 namespace Bottles.Services.Messaging.Tracking
 {
-    public class MessageSent
+    public static class MessageHistory
     {
-        public string FullName { get; set; }
-        public string Type { get; set; }
-        public DateTime Timestamp { get; set; }
-        public string Id { get; set; }
-        public string Description { get; set; }
+        
     }
 
-    public class MessageReceived
+    public class MessageTrack
     {
+        public static readonly string Sent = "Sent";
+        public static readonly string Received = "Received";
+
+
+
+        public static MessageTrack ForSent(object message, string id = null)
+        {
+            var track = derive(message, id);
+            track.Status = Sent;
+            return track;
+        }
+
+        private static MessageTrack derive(object message, string id)
+        {
+            var messageType = message.GetType();
+            var track = new MessageTrack
+            {
+                Description = message.ToString(),
+                FullName = messageType.FullName,
+                Type = messageType.Name,
+                Timestamp = DateTime.UtcNow,
+                Id = id
+            };
+
+            if (id.IsEmpty())
+            {
+                autodetermineId(message, messageType, track);
+            }
+
+            return track;
+        }
+
+        private static void autodetermineId(object message, Type messageType, MessageTrack track)
+        {
+            var property = messageType.GetProperties().FirstOrDefault(x => x.Name.EqualsIgnoreCase("Id"));
+            if (property != null)
+            {
+                var rawValue = property.GetValue(message, null);
+                if (rawValue != null)
+                {
+                    track.Id = rawValue.ToString();
+                }
+            }
+        }
+
+        public static MessageTrack ForReceived(object message, string id = null)
+        {
+            var track = derive(message, id);
+            track.Status = Received;
+            return track;
+        }
+
         public string FullName { get; set; }
         public string Type { get; set; }
         public DateTime Timestamp { get; set; }
         public string Id { get; set; }
         public string Description { get; set; }
+
+        public string Status { get; set; }
+
+        
     }
+
 }
