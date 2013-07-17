0c1b12442b193ffaeb8428f2ad346af155ad46f8
jeremydmiller <jeremydmiller@yahoo.com>
2013-07-17 08:49:38 -0500

    moved extensions out into its own file

--- src/Bottles.Services/Messaging/IMessagingHub.cs
+++ src/Bottles.Services/Messaging/IMessagingHub.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace Bottles.Services.Messaging
 {
@@ -10,30 +9,5 @@ namespace Bottles.Services.Messaging
         void RemoveListener(object listener);
         void Send<T>(T message);
         void SendJson(string json);
-    }
-
-    public static class MessagingHubExtensions
-    {
-        public static T WaitForMessage<T>(this IMessagingHub hub, Func<T, bool> filter, Action action, int wait = 5000)
-        {
-            var condition = new MessageWaitCondition<T>(filter);
-            hub.AddListener(condition);
-            action();
-
-            try
-            {
-                return condition.Wait(wait);
-            }
-            finally
-            {
-                hub.RemoveListener(condition);
-            }
-        }
-
-        public static T WaitForMessage<T>(this IMessagingHub hub, Action action, int wait = 5000)
-        {
-            Func<T, bool> filter = x => true;
-            return hub.WaitForMessage(filter, action, wait);
-        }
     }
 }
